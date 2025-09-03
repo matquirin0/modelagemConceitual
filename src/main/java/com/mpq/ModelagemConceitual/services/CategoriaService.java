c44699cbb14cbc23662e7e73e41a92afcdbade10
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-09-03 09:50:33 -0300

    Realizando ajuster no endpoint /categorias/{id}

--- src/main/java/com/mpq/ModelagemConceitual/services/CategoriaService.java
+++ src/main/java/com/mpq/ModelagemConceitual/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.mpq.modelagemconceitual.domain.Categoria;
 import com.mpq.modelagemconceitual.repositories.CategoriaRepository;
+import com.mpq.modelagemconceitual.services.exceptions.ObjectNotFoundException;
 
 
 @Service
@@ -17,6 +18,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
 	}
