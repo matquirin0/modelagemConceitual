2077179be47b50b8ed41556e7033f50390cde47e
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-09-03 09:25:45 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/mpq/ModelagemConceitual/ModelagemConceitualApplication.java
+++ src/main/java/com/mpq/ModelagemConceitual/ModelagemConceitualApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mpq.modelagemconceitual.domain.Categoria;
+import com.mpq.modelagemconceitual.domain.Produto;
 import com.mpq.modelagemconceitual.repositories.CategoriaRepository;
+import com.mpq.modelagemconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemConceitualApplication implements CommandLineRunner {
 	
 	@Autowired
 	public CategoriaRepository categoriaRepository;
+	@Autowired
+	public ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -26,7 +30,20 @@ public class ModelagemConceitualApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto (null, "Impressora", 800.00);
+		Produto p3 = new Produto (null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
